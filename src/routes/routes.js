2ede7ccec6ccf57d6d16573efb249dc13a74d398
thinh <lqt542001@gmail.com>
2023-03-30 00:29:26 +0700

    upload source code

---
+++ src/routes/routes.js
@@ -0,0 +1,28 @@
+import config from '~/config';
+
+// Layouts
+import { HeaderOnly } from '~/layouts';
+
+// Pages
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+import Live from '~/pages/Live';
+
+// Public routes
+// những routes không cần đăng nhập vẫn vào được
+const publicRoutes = [
+    { path: config.routes.home, component: Home },
+    { path: config.routes.following, component: Following },
+    { path: config.routes.live, component: Live },
+    { path: config.routes.profile, component: Profile },
+    { path: config.routes.upload, component: Upload, layout: HeaderOnly },
+    { path: config.routes.search, component: Search, layout: null },
+];
+
+// Những routes cần đăng nhập mới vào được
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
